05dbfe908b22a6f334e0aad04bae000bc8e341a6
Celes Renata <celes@celestium.life>
2024-02-22 11:49:50 -0800

    small updates for when reloading ags

--- celes-dots-install.sh
+++ celes-dots-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-yay -S --needed --noconfirm touchegg-git gnome-pie-git
+yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync
 sudo systemctl enable touchegg
 sudo systemctl start touchegg
+rsync -aHx .confg/ $HOME/.config/
