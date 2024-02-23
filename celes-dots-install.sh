b0428391e87fe52f5615158f796fad2447972a67
Celes Renata <celes@celestium.life>
2024-02-23 06:12:20 -0800

    fix typo

--- celes-dots-install.sh
+++ celes-dots-install.sh
@@ -9,7 +9,7 @@ cd ~/
 yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync cairo-dock-plug-ins-wayland-git
 sudo systemctl enable touchegg
 sudo systemctl start touchegg
-rsync -aHx .confg $HOME/
+rsync -aHx .config ~/
 cd ~/sources
 git clone https://github.com/RedBearAK/toshy.git
 cd toshy
