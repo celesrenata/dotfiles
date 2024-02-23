905a25d51c2fdc9bf89f9aabb14cd0afaf75465e
Celes Renata <celes@celestium.life>
2024-02-22 23:59:17 -0800

    minor update

--- celes-dots-install.sh
+++ celes-dots-install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+mkdir -p ~/sources/}end-4,celes}
+cd ~/sources/end-4
+git clone https://github.com/end-4/dots-hyprland.git
+cd dots-hyprland
+./install
+cd ~/
 yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync cairo-dock-plug-ins-wayland-git
 sudo systemctl enable touchegg
 sudo systemctl start touchegg
 rsync -aHx .confg/ $HOME/.config/
-mkdir ~/sources
 cd ~/sources
 git clone https://github.com/RedBearAK/toshy.git
 cd toshy
