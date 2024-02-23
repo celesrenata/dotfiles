2991bebdf3a8bf48d69808a27f539391679235f2
Celes Renata <celes@celestium.life>
2024-02-23 00:10:34 -0800

    fix typos

--- celes-dots-install.sh
+++ celes-dots-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p ~/sources/}end-4,celes}
+mkdir -p ~/sources/{end-4,celes}
 cd ~/sources/end-4
 git clone https://github.com/end-4/dots-hyprland.git
 cd dots-hyprland
-./install
+./install.sh
 cd ~/
 yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync cairo-dock-plug-ins-wayland-git
 sudo systemctl enable touchegg
