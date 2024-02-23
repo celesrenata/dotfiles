32b8bb63b11018e57324f8a1cfdcbfdf046c0f1e
Celes Renata <celes@celestium.life>
2024-02-22 23:45:40 -0800

    add cairo-dock

--- celes-dots-install.sh
+++ celes-dots-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync
+yay -S --needed --noconfirm touchegg-git gnome-pie-git wofi rsync cairo-dock-plug-ins-wayland-git
 sudo systemctl enable touchegg
 sudo systemctl start touchegg
 rsync -aHx .confg/ $HOME/.config/
+mkdir ~/sources
+cd ~/sources
+git clone https://github.com/RedBearAK/toshy.git
+cd toshy
+./setup_toshy.py install --override-distro arch
+rm -rf ~/.config/cairo-dock/current_theme/plug-ins/switcher/
