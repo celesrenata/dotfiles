f2976a649ec2a23c8db0f41499065817720609e2
Celes Renata <celes@celestium.life>
2024-04-06 20:31:12 -0700

    allow scale adjustment

--- .local/bin/initialSetup.sh
+++ .local/bin/initialSetup.sh
@@ -2,5 +2,7 @@
 if ! [ -d ~/.config/foot ]; then
   mkdir -p ~/.config/foot
   mkdir -p ~/.config/fuzzel
+  read -p "Enter Scale (#.#): " SCALE
+  echo "monitor=,preferred,auto,${SCALE}" >> ~/.config/hypr/custom/custom.conf
   ~/.config/ags/scripts/color_generation/switchwall.sh
 fi
