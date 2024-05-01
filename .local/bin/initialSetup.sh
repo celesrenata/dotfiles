6b33c397cc6ca27a6035a6ef881c3bad3e91d4d5
Celes Renata <celes@celestium.life>
2024-04-30 19:00:10 -0700

    fix sunshine start

--- .local/bin/initialSetup.sh
+++ .local/bin/initialSetup.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
+imgpath=~/Backgrounds/love-is-love.jpg
+cursorposx=$(hyprctl cursorpos -j | gojq '.x' 2>/dev/null) || cursorposx=960
+cursorposy=$(hyprctl cursorpos -j | gojq '.y' 2>/dev/null) || cursorposy=540
+cursorposy_inverted=$(( screensizey - cursorposy ))
 if ! [ -f ~/.local/share/initialSetup ]; then
   mkdir -p ~/.local/share
   mkdir -p ~/.config/foot
   mkdir -p ~/.config/fuzzel
-  #read -p "Enter Scale (#.#): " SCALE
+  mkdir -p ~/Videos
   echo "monitor=,preferred,auto,1" > ~/.config/hypr/custom/custom.conf
-  ~/.config/ags/scripts/color_generation/switchwall.sh
+  swww img "$imgpath" --transition-step 100 --transition-fps 60 \
+	  --transition-type grow --transition-angle 30 --transition-duration 2 \
+	  --transition-pos "$cursorposx, $cursorposy_inverted"
+  ~/.config/ags/scripts/color_generation/colorgen.sh "${imgpath}" --apply --smart
   if [ $? -eq 0 ]; then
     touch ~/.local/share/initialSetup
   fi
