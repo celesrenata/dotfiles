a374624ffb2bb40b3942c1c451ad46fefff33d46
Celes Renata <celes@celestium.life>
2024-05-05 22:45:42 -0700

    add extra line for RPI setup

--- .local/bin/initialSetupRPI5.sh
+++ .local/bin/initialSetupRPI5.sh
@@ -4,6 +4,7 @@ if ! [ -f ~/.local/share/initialSetup ]; then
   mkdir -p ~/.config/foot
   mkdir -p ~/.config/fuzzel
   echo "monitor=,1920x1080@60,auto,1" > ~/.config/hypr/custom/custom.conf
+  echo "exec-once = swww init; swww kill; swww init" >> ~/.config/hypr/custom/custom.conf
   ~/.config/ags/scripts/color_generation/switchwall.sh
   if [ $? -eq 0 ]; then
     touch ~/.local/share/initialSetup
